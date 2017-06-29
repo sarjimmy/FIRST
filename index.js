bdbda028a7bdba98d669bc2d0a12b4a5178df1b0
Ubuntu <ubuntu@ip-172-31-31-224.us-east-2.compute.internal>
2017-06-29 08:13:47 +0000

    Got map displayed

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT_NUMBER = 8080;
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/mymap.html'));})
+
+app.listen(PORT_NUMBER,function(){
+  console.log('Example app listening on port ' + PORT_NUMBER);})
+
+
