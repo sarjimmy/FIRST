3843437d6e6d6cd24d4c40a3c33fd181bf2180f9
Ubuntu <ubuntu@ip-172-31-31-224.us-east-2.compute.internal>
2017-06-29 09:37:36 +0000

    trying to combine external css file

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT_NUMBER = 8080;
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/mymap.html'));})
 
+app.use('/css',express.static('css'));
+
 app.listen(PORT_NUMBER,function(){
   console.log('Example app listening on port ' + PORT_NUMBER);})
 
